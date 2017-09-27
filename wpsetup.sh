28521ed50b86a7702aed3ef54fa155aa93ce3645
shahaji <theofficialshahajirao@gmail.com>
2017-09-28 01:08:29 +0530

    Generate log files The script now generates the log files in a folder-"logs". Now the execution of the script is clean and not cluttered like before.

--- wpsetup.sh
+++ wpsetup.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
-
+wplog=wordpress.log
+mysqllog=mysql.log
+nginxlog=nginx.log
+installlog=installation.log
 #installation check
 installation_check(){
-	echo "Checking Installation of $1"
+	echo "Checking Installation of $1..."
 	dpkg --status $1 >> /dev/null
 	if [ $? -ne 0 ]
 	then
-		echo "Installing $1"
-		sudo apt-get install $1 -y
+		echo "Installing $1..."
+		sudo apt-get install $1 -y &>> $installlog 
 		if [ $? -eq 0 ]
 		then
-			echo "Successfully Installed $1"
+			echo "Successfully Installed $1..."
 		else
 			echo "Error while installing $1. Exiting..."
 			exit 1
 		fi
 	else
-		echo "$1 already installed"
+		echo "$1 is already installed..."
 	fi	
 }
 
 #domain name entry 
 get_domain_name(){
 	read -p "Enter Domain Name: " domain
-	echo "127.0.0.1         $domain" | sudo tee -a  /etc/hosts
+	echo "127.0.0.1         $domain" | sudo tee -a  /etc/hosts &>> $wplog
 	if [ $? -eq 0 ]
 	then
 		echo "Domain name entry successful"
@@ -35,7 +38,7 @@ get_domain_name(){
 
 #setting up nginx config file for the site
 set_nginx_file(){
-	echo "Setting up nginx config file for your site"
+	echo "Setting up nginx config file for your site..."
 	sudo cp nginx.conf /etc/nginx/sites-available/$domain
 	sudo sed -i "s/domain_name/$domain/g" /etc/nginx/sites-available/$domain
 	sudo ln -s /etc/nginx/sites-available/$domain /etc/nginx/sites-enabled/
@@ -43,7 +46,7 @@ set_nginx_file(){
 	if [ $? -eq 0 ]
 	then	
 		echo "Successfully set up nginx config file"
-		sudo systemctl reload nginx
+		sudo systemctl reload nginx &>> $nginxlog
 	else
 		echo "Some error occured during nginx configuration"
 		exit 1
@@ -53,10 +56,10 @@ set_nginx_file(){
 
 #Download Wordpress 
 wordpress_download(){
-	echo "Downloading Wordpress"
-	sudo wget -O wordpress.zip http://wordpress.org/latest.zip
+	echo "Downloading Wordpress..."
+	sudo wget -O wordpress.zip http://wordpress.org/latest.zip &>> $wplog
 	installation_check unzip
-	sudo unzip -q wordpress.zip
+	sudo unzip -q wordpress.zip &>> $wplog
 	sudo rm -f wordpress.zip
 	sudo mkdir /var/www/$domain
 	sudo mv wordpress/* /var/www/$domain
@@ -67,25 +70,25 @@ wordpress_download(){
 #MySQL installation
 mysql_installation(){
 	#storing temporary root password for mysql
-	sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password password"
-	sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password password"
+	sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password password" &>> $mysqllog
+	sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password password" &>> $mysqllog
 	installation_check mysql-server
 	#erasing the saved password
-	sudo debconf-communicate mysql-server <<< 'PURGE'
+	sudo debconf-communicate mysql-server <<< 'PURGE' &>> $mysqllog
 }
 
 #Create site default database 
 mysql_create_db(){
-	echo "Creating database for your site"
+	echo "Creating database for your site..."
 	db_ext="_db"
 	db_name=${domain//./_}$db_ext
-	mysql -u root -ppassword -e "create database $db_name;"
+	mysql -u root -ppassword -e "create database $db_name;" &>> $mysqllog
 	echo "Database Created"
 }
 
 #Setting Up wp-config.php
 wpconfig_setup(){
-	echo "Setting up the wp-config.php file"
+	echo "Setting up the wp-config.php file..."
 	sudo cp wp-config.php /var/www/$domain/
 	sudo sed -i "s/database_name_here/$db_name/g" /var/www/$domain/wp-config.php
 	sudo sed -i "s/username_here/root/g" /var/www/$domain/wp-config.php
@@ -94,7 +97,8 @@ wpconfig_setup(){
 }
 
 #update package-list 
-sudo apt-get update
+echo "Updating system. This may take time..."
+sudo apt-get update &>> $installlog
 
 
 installation_check php7.0-fpm
@@ -107,7 +111,8 @@ wordpress_download
 mysql_create_db
 wpconfig_setup
 
-
+mkdir logs
+mv *.log logs/
 echo "Database username: root"
 echo "Database password: password"
 echo "Open your site "$domain" in the browser"
